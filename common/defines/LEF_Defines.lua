a9a5022ed7662edff9207dae94ddf39298f07b0a
Greenbueller <pennygaming10@gmail.com>
2022-08-12 23:31:47 -0400

    Date Moment
    A Wilted Tulip Bookmark now properly established, attempting to fix a colour not showing in VSC [Didn't work], and updating defines for the new start date plus some other changes. See edited files for full details.

---
+++ common/defines/LEF_Defines.lua
@@ -0,0 +1,30 @@
+--- LEF - Legacy of '48 Defines
+
+--- Dates ---
+
+NDefines.NGame.START_DATE = "1910.1.1.12"
+NDefines.NGame.END_DATE = "1925.1.1.1"
+
+--- Diplomacy ---
+
+NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "1910.1.1.12"
+NDefines.NDiplomacy.IDEOLOGY_JOIN_FACTION_MIN_LEVEL = "0.25" -- Usually .30
+
+--- Country ---
+
+NDefines.NCountry.WEEKLY_STABILITY_GAIN = "0.0025"
+NDefines.NCountry.WEEKLY_WAR_SUPPORT_GAIN = "0.0025"
+NDefines.NCountry.DEFAULT_STABILITY = "0.45" -- Usually .50
+NDefines.NCountry.DEFAULT_WAR_SUPPORT = "0.45" -- Usually .50
+NDefines.NCountry.MIN_STABILITY = "-0.50"
+NDefines.NCountry.MIN_WAR_SUPPORT = "-0.50"
+NDefines.NCountry.STARTING_COMMAND_POWER = "5.0"
+
+--- AI ---
+
+NDefines.NAI.DIPLOMACY_FACTION_WAR_WANTS_HELP = "40"
+
+--- Focuses ---
+
+NDefines.NFocus.FOCUS_POINT_DAYS = "1" -- Focus time is now per day rather than per week.
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = "14"
